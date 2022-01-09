58451bcd653ace67751d83edbe63ac3e36ad6583
Marko Mosic <markomosic90@gmail.com>
2022-01-09 12:26:53 +0100

    Project completed

--- src/projekat_1/ShopAndGo.java
+++ src/projekat_1/ShopAndGo.java
@@ -15,6 +15,28 @@ public class ShopAndGo {
 				"Kutija" };
 		int[] price = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
 
+		int index = 0;
+		int totalPrice = 0;
+		System.out.println("Welcome!");
+		while (index >= 0) {
+			System.out.print("Add ingredient to your pasta: ");
+			index = findIngredient(ingredients, s.nextLine());
+			if (index >= 0 && index < 16) {
+				totalPrice = totalPrice + price[index];
+			} else if (index == 16) {
+				System.out.println("Ingredient doesn't exist, choose other ingredient!");
+			}
+		}
+
+		System.out.print("Enter your phone number: ");
+		if (isRegularCustomer(regularCustomers, s.nextLine())) {
+			totalPrice = totalPrice - (totalPrice * 10) / 100;
+		}
+
+		System.out.println("Total price: " + totalPrice);
+
+		System.out.println("Bon Appetit!");
+
 	}
 
 	public static int findIngredient(String[] ingredients, String ingredient) {
@@ -30,7 +52,7 @@ public class ShopAndGo {
 			return index = 16;
 		}
 	}
-	
+
 	public static boolean isRegularCustomer(String[] regularCustomers, String phoneNumber) {
 		for (int i = 0; i < regularCustomers.length; i++) {
 			if (regularCustomers[i].equals(phoneNumber)) {
