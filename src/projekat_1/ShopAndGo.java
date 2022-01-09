247ad55e54be3dadc939cb37d526d5293a8e750c
Marko Mosic <markomosic90@gmail.com>
2022-01-09 12:03:24 +0100

    New arrays in main

---
+++ src/projekat_1/ShopAndGo.java
@@ -0,0 +1,20 @@
+package projekat_1;
+
+import java.util.Scanner;
+
+public class ShopAndGo {
+
+	public static void main(String[] args) {
+//		Pasta Bar
+		
+		Scanner s = new Scanner(System.in);
+
+		String[] regularCustomers = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+		String[] ingredients = { "Makarone", "Spagete", "Bolognes", "Curetina", "Govedja prsuta", "Slanina", "Piletina",
+				"4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix", "Pecurke",
+				"Kutija" };
+		int[] price = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+
+	}
+
+}
